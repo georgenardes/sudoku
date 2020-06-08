728a678f10383a3dd9d945019a203ec147b34c04
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-06-08 11:37:18 -0300

    primeiros passos das verificações linha e coluna

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 
 using namespace std;
+#define TAM 9
+
+bool verificaColuna(int mat[][TAM], int x, int y, int valor){
+
+   for(int i = 0; i < TAM; i ++){
+        if(mat[i][y] == valor)
+            return false;
+   }
+   return true;
+}
+
+bool verificaLinha(int mat[][TAM], int x, int y, int valor){
+
+   for(int i = 0; i < TAM; i ++){
+        if(mat[x][i] == valor)
+            return false;
+   }
+   return true;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int mat[TAM][TAM]={0,0,0,0,0,0,0,0,0
+                       0,0,2,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0
+                       0,0,0,0,0,0,0,0,0};
+
+    valor = 2
+   // printf("retornou", verificaLinha(mat,tam,1,2,valor));
+
+
+
+
+
     return 0;
 }
