65cf18fe6eb3374b42fda2ca10177261eb31584e
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-06-08 12:33:42 -0300

    funcionando verificação coluna e linha

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
 using namespace std;
-#define TAM 9
 
-bool verificaColuna(int mat[][TAM], int x, int y, int valor){
+int **Alocar_matriz_real (int qtdVert){
+    int** matriz = new int*[qtdVert];
+
+    for (int i = 0; i < qtdVert; ++i)
+        matriz[i] = new int[qtdVert];
+
+    return (matriz);
+}
+
+bool verificaColuna(int **mat, int TAM, int x, int y, int cor){
 
    for(int i = 0; i < TAM; i ++){
-        if(mat[i][y] == valor)
+        if(mat[i][y] == cor)
             return false;
    }
    return true;
 }
 
-bool verificaLinha(int mat[][TAM], int x, int y, int valor){
+bool verificaLinha(int **mat, int TAM, int x, int y, int cor){
 
    for(int i = 0; i < TAM; i ++){
-        if(mat[x][i] == valor)
+        if(mat[x][i] == cor)
             return false;
    }
    return true;
@@ -23,18 +31,28 @@ bool verificaLinha(int mat[][TAM], int x, int y, int valor){
 
 int main()
 {
-    int mat[TAM][TAM]={0,0,0,0,0,0,0,0,0
-                       0,0,2,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0
-                       0,0,0,0,0,0,0,0,0};
+    int TAM = 9;
+    int **mat;
+    mat = Alocar_matriz_real(TAM);
 
-    valor = 2
-   // printf("retornou", verificaLinha(mat,tam,1,2,valor));
+    for (int i = 0; i < TAM; i++)// zerando matriz
+        for (int j = 0; j < TAM; j++)
+            mat[i][j] = 0 ;
+
+    mat[1][2] = 2;//colocando um valor para testar as verificações
+
+    int cor = 2;
+    if(verificaColuna(mat,TAM,0,2,cor)==true)
+        cout << "pode inserir neste lugar" << endl;
+    else
+
+        cout << "valor ja existente" << endl ;
+
+    int cor2 = 2;
+    if(verificaLinha(mat,TAM,3,0,cor2)==true)
+        cout << "pode inserir neste lugar" << endl;
+    else
+        cout << "valor ja existente" << endl;
 
 
 
