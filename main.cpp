fa15821759839fc463479e586ac3207496dd3ee8
georgenardes <georgenardes@gmail.com>
2020-06-08 22:09:27 -0300

    prenche matriz adj

--- main.cpp
+++ main.cpp
@@ -1,62 +1,90 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int **Alocar_matriz_real (int qtdVert){
-    int** matriz = new int*[qtdVert];
+bool **Alocar_matriz_real (int qtdVert){
+    bool** matriz = new bool*[qtdVert];
 
     for (int i = 0; i < qtdVert; ++i)
-        matriz[i] = new int[qtdVert];
+        matriz[i] = new bool[qtdVert];
 
     return (matriz);
 }
 
-bool verificaColuna(int **mat, int TAM, int x, int y, int cor){
+bool verificaColuna(int **mat, int qtd_vert, int x, int y, int cor){
 
-   for(int i = 0; i < TAM; i ++){
+   for(int i = 0; i < qtd_vert; i ++){
         if(mat[i][y] == cor)
             return false;
    }
    return true;
 }
 
-bool verificaLinha(int **mat, int TAM, int x, int y, int cor){
+bool verificaLinha(int **mat, int qtd_vert, int x, int y, int cor){
 
-   for(int i = 0; i < TAM; i ++){
+   for(int i = 0; i < qtd_vert; i ++){
         if(mat[x][i] == cor)
             return false;
    }
    return true;
 }
 
+
+void monta_matriz_adj(bool **_matrizAdjacencia, int qtd_vert){
+    // quantidade de linhas/colunas do sudoku
+    int _ordem = sqrt(qtd_vert);
+
+    // quantidade de linhas de um quadrante
+    int _ordem0 = sqrt(_ordem);
+
+    for (int i = 0; i < qtd_vert; i++){
+        for (int j = 0; j < qtd_vert; j++){
+            if (j >= (i / _ordem) * _ordem && j < (i / _ordem) * _ordem + _ordem)
+                _matrizAdjacencia[i][j] = 1;
+            else if (j % _ordem == i % _ordem)
+                _matrizAdjacencia[i][j] = 1;
+            else
+                _matrizAdjacencia[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < qtd_vert; i++){
+        for (int f = 0; f < _ordem0; f++){
+            for (int k = 0; k < _ordem0; k++){
+                if ((i / _ordem) % _ordem0 == 0)
+                    for (int j = 0; j < _ordem0; j++){
+                        _matrizAdjacencia[i + _ordem * f][(i / _ordem0) * _ordem0 + j + _ordem * k] = 1;
+                    }
+            }
+        }
+    }
+}
+
+
 int main()
 {
-    int TAM = 9;
-    int **mat;
-    mat = Alocar_matriz_real(TAM);
+    int qtd_vert = 16;
 
-    for (int i = 0; i < TAM; i++)// zerando matriz
-        for (int j = 0; j < TAM; j++)
-            mat[i][j] = 0 ;
+    bool **_matrizAdjacencia; // matriz adjacencia
 
-    mat[1][2] = 2;//colocando um valor para testar as verificações
+    _matrizAdjacencia = Alocar_matriz_real(qtd_vert);
 
-    int cor = 2;
-    if(verificaColuna(mat,TAM,0,2,cor)==true)
-        cout << "pode inserir neste lugar" << endl;
-    else
+    for (int i = 0; i < qtd_vert; i++)// zerando matriz
+        for (int j = 0; j < qtd_vert; j++)
+            _matrizAdjacencia[i][j] = 0;
 
-        cout << "valor ja existente" << endl ;
+    monta_matriz_adj(_matrizAdjacencia, qtd_vert);
 
-    int cor2 = 2;
-    if(verificaLinha(mat,TAM,3,0,cor2)==true)
-        cout << "pode inserir neste lugar" << endl;
-    else
-        cout << "valor ja existente" << endl;
-
-
-
-
+    // ------------ printa matriz ---------------- //
+    int i = 0;
+    int j = 0;
+    cout<< " --------- Matriz de adjacencia --------- "<<endl;
+    for (i=0; i < qtd_vert; i++){
+        for (j=0; j < qtd_vert; j++)
+            cout << _matrizAdjacencia[i][j] << " ";
+        cout << endl;
+    }
 
     return 0;
 }
